7fadc94e354d0ffff98680fdb2f06ffdcb1570b4
Nelson Nobre <technobre@gmail.com>
2023-11-12 03:47:19 +0000

    Demo Inline Arrays

---
+++ src/InlineArrays/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var buffer = new Buffer();
+for (var i = 0; i < 10; i++)
+{
+    buffer[i] = i * i;
+    Console.WriteLine($"buffer[{i}] = {buffer[i]}");
+}
+
+
+[System.Runtime.CompilerServices.InlineArray(10)]
+public struct Buffer
+{
+    private int _element0;
+}
