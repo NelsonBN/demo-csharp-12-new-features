6b5bb921e8ccd35f7dfebfe5542977c282546346
Nelson Nobre <technobre@gmail.com>
2023-11-16 22:31:17 +0000

    More primary constructors examples

--- src/PrimaryConstructors/Program.cs
+++ src/PrimaryConstructors/Program.cs
@@ -1,4 +1,7 @@
-﻿var user = new User("John", 42);
+﻿// Example of primary constructors with classes and structs
+using Microsoft.Extensions.DependencyInjection;
+
+var user = new User("John", 42);
 Console.WriteLine($"Name: {user.Name} - Age: {user.Age}");
 
 var car = new Car("Mustang", 1969);
@@ -6,6 +9,18 @@ car.Print();
 
 var person = new Person(Guid.NewGuid(), "John", 42);
 person.Print();
+
+
+// Example of primary constructors with Dependency Injection
+var services = new ServiceCollection();
+services.AddSingleton<MyService>();
+services.AddSingleton<DateFormatter>();
+
+var provider = services.BuildServiceProvider();
+
+var myService = provider.GetRequiredService<MyService>();
+myService.Run();
+
 
 
 public class User(string Name, int Age)
@@ -27,5 +42,22 @@ public struct Person(Guid Id, string Name, int Age)
     public void Print()
     {
         Console.WriteLine($"Id: {Id} - Name: {Name} - Age: {Age}");
+    }
+}
+
+// Be careful because the properties in the primary constructor aren't readonly (They are mutable)
+public class MyService(DateFormatter Formatter)
+{
+    public void Run()
+    {
+        Console.WriteLine($"Today is {Formatter.Format(DateTime.Now)}");
+    }
+}
+
+public class DateFormatter()
+{
+    public string Format(DateTime date)
+    {
+        return date.ToString("dd/MM/yyyy");
     }
 }
